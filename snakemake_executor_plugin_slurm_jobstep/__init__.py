bf85e3f0bfb7e9fb2aa6018d4da8a29ef22bf1d2
meesters <meesters@uni-mainz.de>
2026-03-11 10:30:12 +0100

    fix: attempt to remove array tasks from first task more elegantly (might not truncate something unwanted

--- snakemake_executor_plugin_slurm_jobstep/__init__.py
+++ snakemake_executor_plugin_slurm_jobstep/__init__.py
@@ -77,7 +77,6 @@ class ExecutorSettings(ExecutorSettingsBase):
     )
 
 
-
 # Required:
 # Implementation of your executor
 class Executor(RealExecutor):
@@ -88,7 +87,7 @@ class Executor(RealExecutor):
         # we consider this job to be a GPU job, if a GPU has been reserved
         self.gpu_job = os.getenv("SLURM_GPUS")
         # check if SLURM_ARRAY_TASK_ID is set, to determine whether this is a job array task
-        self.job_array_task = os.getenv("SLURM_ARRAY_TASK_ID") is not None   
+        self.job_array_task = os.getenv("SLURM_ARRAY_TASK_ID") is not None
 
     def run_job(self, job: JobExecutorInterface):
         # Implement here how to run a job.
@@ -157,21 +156,29 @@ class Executor(RealExecutor):
             min_array_index = min(int(key) for key in array_execs.keys())
             if array_index == min_array_index:
                 # in this case we need to pass the exec strings of
-                # as for a single job, but with the extracted 
+                # as for a single job, but with the extracted
                 # --slurm-jobstep-array-execs flag
                 call = self.format_job_exec(job)
-                index = call.find("--slurm-jobstep-array-execs")
-                if index != -1:
-                    call = call[:index]
-                self.logger.debug(f"Handling first job array task with index {array_index}")
+                # index = call.find("--slurm-jobstep-array-execs")
+                # if index != -1:
+                #    call = call[:index]
+                # Remove the --slurm-jobstep-array-execs flag and its value
+                call = re.sub(r"--slurm-jobstep-array-execs\s+\S+\s*", "", call)
+                self.logger.debug(
+                    f"Handling first job array task with index {array_index}"
+                )
                 self.logger.debug(f"Raw call for first array index: {call}")
             else:
                 self.logger.debug(f"Handling job array task with index {array_index}")
-                self.logger.debug(f"Raw array execs: {self.workflow.executor_settings.array_execs}") 
+                self.logger.debug(
+                    f"Raw array execs: {self.workflow.executor_settings.array_execs}"
+                )
                 compressed_hex = array_execs[str(array_index)]
                 compressed_bytes = bytes.fromhex(compressed_hex)
                 call = zlib.decompress(compressed_bytes).decode("utf-8")
-                self.logger.debug(f"Decompressed call for array index {array_index}: {call}")
+                self.logger.debug(
+                    f"Decompressed call for array index {array_index}: {call}"
+                )
         else:
             # SMP job, execute snakemake with srun, to ensure proper placing of threaded
             # executables within the c-group
