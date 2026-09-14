cf6a83c3666fdb1b77e53facfbac3980add20137
Christian Meesters <meesters@csc.uni-frankfurt.de>
2026-09-14 13:20:18 +0200

    fix: formatting

--- snakemake_executor_plugin_slurm_jobstep/__init__.py
+++ snakemake_executor_plugin_slurm_jobstep/__init__.py
@@ -149,8 +149,9 @@ class Executor(RealExecutor):
             self.logger.debug(
                 f"Checking input file {inputfile} with flags {inputfile.flags}"
             )
-            self.logger.debug("is_ondemand_eligible: "
-                              f"{is_ondemand_eligible(inputfile)}")
+            self.logger.debug(
+                f"is_ondemand_eligible: {is_ondemand_eligible(inputfile)}"
+            )
             if is_ondemand_eligible(inputfile) and self.node_local_prefix:
                 # if the file size is < 2GB, we use sbcast, otherwise scp
                 size = get_file_size(inputfile)
